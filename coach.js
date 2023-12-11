d3dd62fe94b29a41ab798ad1f5afe5dd2168eb2d
MIIkasxc <153520431+MIIkasxc@users.noreply.github.com>
2023-12-12 08:25:53 +0900

    Delete coach.js

--- coach.js
+++
@@ -1,8 +0,0 @@
-function dlfile(code){
-    doubleSubmitFlag = false;
-    stop_chk = false;
-    let para = document.location.href.split("?");
-	location.href = 'https://vfile.nscoach.com/file/free.downloadFile.php?it_id='+code+'&re=null&file_type=1&para='+para;
-}
-
-dlfile("datainput")
